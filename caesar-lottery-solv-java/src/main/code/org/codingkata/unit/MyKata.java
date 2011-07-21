e004430ed063e9c94acf4b0ad2d3ccc8bde3b288
rafaeldx7 <rafaeldx7@gmail.com>
2011-07-21 03:53:28 -0300

    caesar-lottery-solv-java

---
+++ caesar-lottery-solv-java/src/main/code/org/codingkata/unit/MyKata.java
@@ -0,0 +1,19 @@
+package org.codingkata.unit;
+
+import org.codingkata.unit.api.BaseKataSolution;
+
+public class MyKata extends BaseKataSolution {
+
+    /**
+     * Convert an Arabic numeral to Roman numeral
+     *
+     * @param number    an Arabic numeral
+     * @return          the Roman numeral equivalent to the Arabic numeral
+     */
+    public String convert (int number) {
+        String[] units = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+        String[] tens   = { "X", "XX", "XXX", "XL", "L" };
+
+        return (number > 10) ? tens[(number / 10) - 1] + units[(number % 10) - 1] : units[number - 1];
+    }
+}
