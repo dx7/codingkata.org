7172199c765b08ae15a189e28a27967781ac5a24
rafaeldx7 <rafaeldx7@gmail.com>
2011-07-21 19:57:16 -0300

    solved mars-books-solv-java

--- mars-books-solv-java/src/main/code/org/codingkata/unit/MyKata.java
+++ mars-books-solv-java/src/main/code/org/codingkata/unit/MyKata.java
@@ -3,6 +3,25 @@ package org.codingkata.unit;
 import org.codingkata.unit.api.BaseKataSolution;
 
 public class MyKata extends BaseKataSolution {
+    /**
+     * Calculate the ISBN-13 check digit
+     *
+     * @param isbn  an incomplete ISBN code (12 characters, no hyphens)
+     * @return      code's check digit
+     */
+    public int calcCheckDigit (String isbn) {
+        int total = 0;
 
-    // YOUR IMPLEMENTATION
+        for (int i = 0; i < isbn.length(); i++) {
+            int digit = Integer.parseInt(String.valueOf(isbn.charAt(i)));
+
+            if (i % 2 == 1) {
+                total += digit * 3;
+            } else {
+                total += digit;
+            }
+        }
+
+        return 10 - total % 10;
+    }
 }
