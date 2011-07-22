d0085f0753e95318eca0fa5509d45be6b1b0b3ed
rafaeldx7 <rafaeldx7@gmail.com>
2011-07-22 02:03:55 -0300

    solved happy-number-solv-java

--- happy-number-solv-java/src/main/code/org/codingkata/unit/MyKata.java
+++ happy-number-solv-java/src/main/code/org/codingkata/unit/MyKata.java
@@ -1,8 +1,30 @@
 package org.codingkata.unit;
 
 import org.codingkata.unit.api.BaseKataSolution;
+import java.util.ArrayList;
 
 public class MyKata extends BaseKataSolution {
 
-    // YOUR IMPLEMENTATION
+    /**
+     * Check if a number is a 'happy number'
+     *
+     * @param number    number to be checked
+     * @return          true if parameter is a happy number
+     */
+    public boolean isHappy (long number) {
+        long result = number;
+        ArrayList<Long> used = new ArrayList<Long>();
+
+        while (result != 1 && !used.contains(result)) {
+            used.add(result);
+            String number_string = String.valueOf(result);
+            result = 0;
+
+            for (char c: number_string.toCharArray()) {
+                result += Math.pow(Character.getNumericValue(c), 2);
+            }
+        }
+
+        return result == 1;
+    }
 }
