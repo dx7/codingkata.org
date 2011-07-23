868bff313d915a0365d2f45ac49a31f2e9d9f5e2
rafaeldx7 <rafaeldx7@gmail.com>
2011-07-22 22:59:19 -0300

    solved cd-sort-solv-java

--- cd-sort-solv-java/src/main/code/org/codingkata/unit/MyKata.java
+++ cd-sort-solv-java/src/main/code/org/codingkata/unit/MyKata.java
@@ -1,8 +1,18 @@
 package org.codingkata.unit;
 
 import org.codingkata.unit.api.BaseKataSolution;
+import java.util.Collections;
+import java.util.List;
 
 public class MyKata extends BaseKataSolution {
-
-    // YOUR IMPLEMENTATION
+    /**
+     * Sort a bunch of CDs alphabetically by their album title (A-Z)
+     *
+     * @param cds   unsorted list of album titles
+     * @return      sorted list of titles
+     */
+    public List<String> sort (List<String> cds) {
+        Collections.sort(cds); // :D
+        return cds;
+    }
 }
