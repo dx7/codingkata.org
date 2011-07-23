820471005d79caba07b1599f6fb8668f83b81466
rafaeldx7 <rafaeldx7@gmail.com>
2011-07-23 00:30:06 -0300

    save point

--- roman-roulette-solv-java/src/main/code/org/codingkata/unit/MyKata.java
+++ roman-roulette-solv-java/src/main/code/org/codingkata/unit/MyKata.java
@@ -3,6 +3,14 @@ package org.codingkata.unit;
 import org.codingkata.unit.api.BaseKataSolution;
 
 public class MyKata extends BaseKataSolution {
-
-    // YOUR IMPLEMENTATION
+    /**
+     * Calculate the position to stand in order to survive
+     *
+     * @param size      size of the circle
+     * @param skip      persons to skip each round
+     */
+    public int calcSurvivalPosition (int size, int skip) {
+        if (size == 1) return 2;
+        return (calcSurvivalPosition(size - 1, skip) + skip) % size;
+    }
 }
