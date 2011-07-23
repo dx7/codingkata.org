3e869f9f0110df1a376d3190b9c9d4c1c601ac79
rafaeldx7 <rafaeldx7@gmail.com>
2011-07-23 10:18:50 -0300

    solved towers-hanoi-solv-java

--- towers-hanoi-solv-java/src/main/code/org/codingkata/unit/MyKata.java
+++ towers-hanoi-solv-java/src/main/code/org/codingkata/unit/MyKata.java
@@ -3,6 +3,16 @@ package org.codingkata.unit;
 import org.codingkata.unit.api.BaseKataSolution;
 
 public class MyKata extends BaseKataSolution {
-
-    // YOUR IMPLEMENTATION
+    /**
+     * Rearranges the tower by moving all discs from pole 1 to 3
+     */
+    public void rearrange() {
+        move(1, 3);
+        move(1, 2);
+        move(3, 2);
+        move(1, 3);
+        move(2, 1);
+        move(2, 3);
+        move(1, 3);
+    }
 }
